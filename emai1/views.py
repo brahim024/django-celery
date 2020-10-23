1eebd859bdd6822e6f9094383585a00fdb4faa49
brahim024 <ifninos168@gmail.com>
2020-10-23 17:08:20 +0200

    generit revie form

--- emai1/views.py
+++ emai1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from emai1.forms import ReviewForm
+from django.http import HttpResponse
 # Create your views here.
+class ReviewForm(FormView):
+	template_name='review.html'
+	form_class=ReviewForm
+	def form_valid(self,form):
+		form.send_email()
+		msg='Thank for the review !'
+		return HttpResponse(msg)
+
