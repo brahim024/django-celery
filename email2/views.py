00c7c47c0d26f995620fb18263a6e5e7029fc752
brahim024 <ifninos168@gmail.com>
2020-11-04 21:26:08 +0100

    add obj context

--- email2/views.py
+++ email2/views.py
@@ -4,13 +4,13 @@ from django.core.mail import send_mail
 from.forms import SendemailForm
 # Create your views here.
 def send_email(request):
-	obj=Sendemail.object.first()
+	obj=Sendemail.objects.first()
 	if request.method=='POST':
 		subject=request.POST['subject']
 		email=request.POST['email']
 		message=request.POST['message']
 
 		
-		context={'obj':obj}
+	context={'obj':obj}
 	return render(request,'send_form.html',context)
 
