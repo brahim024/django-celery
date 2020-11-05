0d56c4e407cc73cb28d4d201e7396550572036ab
brahim024 <ifninos168@gmail.com>
2020-11-05 01:59:54 +0100

    finish email send

--- email2/views.py
+++ email2/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from.models import Sendemail
 from django.core.mail import send_mail
 from.forms import SendemailForm
+from django.conf import settings
 # Create your views here.
 def send_email(request):
 	obj=Sendemail.objects.first()
@@ -9,17 +10,15 @@ def send_email(request):
 		subject=request.POST['subject']
 		email=request.POST['email']
 		message=request.POST['message']
-		print(subject)
-		print(email)
-		print(message)
 		send_mail(
 		    subject,
 		    message,
 		    settings.EMAIL_HOST_USER,
 		    [email],
 		    fail_silently=False,
-)
+		    
+		)
 		
-	context={'obj':obj}
-	return render(request,'send_form.html',context)
+	
+	return render(request,'send_form.html',{'obj':obj})
 
--- project/settings.py
+++ project/settings.py
@@ -135,6 +135,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER='boughanm6@gmail.com'
 
-EMAIL_HOST_PASSWORD=''
+EMAIL_HOST_PASSWORD='kafaephhihulrpfw'
 EMAIL_POST=587
 EMAIL_USE_TLS=True
